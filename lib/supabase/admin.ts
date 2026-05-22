ee9d29eb1db046a85f676a48ba5aa01bead05dbc
Vanshaj Poonia <vanshajtheunique@gmail.com>
2026-05-22 16:40:35 +0530

    cleanup: remove Supabase runtime stores

--- lib/supabase/admin.ts
+++
@@ -1,8 +0,0 @@
-import { supabaseRequest } from '@/lib/storage/supabase'
-
-export async function supabaseAdminRequest<T>(
-  table: string,
-  init: Parameters<typeof supabaseRequest<T>>[1] = {}
-): Promise<T> {
-  return await supabaseRequest<T>(table, init)
-}
