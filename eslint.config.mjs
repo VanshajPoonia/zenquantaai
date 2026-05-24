afc50b5de874ad6f4ea35100d28fe49de213ea8d
Vanshaj Poonia <vanshajtheunique@gmail.com>
2026-05-24 18:37:24 +0530

    Harden verification tooling

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import { globalIgnores } from 'eslint/config'
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals'
+import nextTypescript from 'eslint-config-next/typescript'
+
+const eslintConfig = [
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  {
+    rules: {
+      'react-hooks/preserve-manual-memoization': 'off',
+      'react-hooks/purity': 'off',
+      'react-hooks/set-state-in-effect': 'off',
+    },
+  },
+  globalIgnores([
+    '.next/**',
+    '.pnpm-store/**',
+    '.storage/**',
+    'build/**',
+    'coverage/**',
+    'next-env.d.ts',
+    'node_modules/**',
+    'out/**',
+  ]),
+]
+
+export default eslintConfig
